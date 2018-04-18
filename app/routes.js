dd44ad69c531d9cf63898b1e261228a0658ea473
laurenceberry <berry.laur@gmail.com>
2018-04-18 17:22:48 +0100

    Set up routes

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,34 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/what_is_verify', function (req, res) {
+  res.render(
+    "what_is_verify/" + req.params.page
+  );
+})
+
+router.get('/check_i_need_verify', function (req, res) {
+  res.render(
+    "check_i_need_verify/" + req.params.page
+  );
+})
+
+router.get('/design_and_build', function (req, res) {
+  res.render(
+    "design_and_build/" + req.params.page
+  );
+})
+
+router.get('/before_you_connect', function (req, res) {
+  res.render(
+    "before_you_connect/" + req.params.page
+  );
+})
+
+router.get('/connect_to_verify', function (req, res) {
+  res.render(
+    "connect_to_verify/" + req.params.page
+  );
+
+})
