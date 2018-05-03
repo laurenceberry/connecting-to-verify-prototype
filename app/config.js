c2c25fb672fe28c867ac144439fffc9c3b57c5e5
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2018-05-03 13:33:28 +0100

    Remove service name from header

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Connect to GOV.UK Verify',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
