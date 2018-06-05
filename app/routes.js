f0c4ecca9a4edf78a814b915107f06327374b203
Tom Hughes <tom.hughes@digital.cabinet-office.gov.uk>
2018-06-05 16:39:53 +0100

    Created first attempt at the matching smart answer

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,37 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.post('/existing-database-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'existing-database'
+  var existingDatabase = req.session.data['existing-database']
+  // Check whether the variable matches a condition
+  if (existingDatabase == "yes"){
+    // Send user to next page
+    res.redirect('/matching/outcome-1-check')
+  }
+  else {
+    // Send user to next question
+    res.redirect('/matching/returning-users')
+  }
+
+})
+
+router.post('/returning-users-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'returning-users'
+  var returningUsers = req.session.data['returning-users']
+  // Check whether the variable matches a condition
+  if (returningUsers == "yes"){
+    // Send user to next page
+    res.redirect('/matching/outcome-2-user-records')
+  }
+  else {
+    // Send user to next question
+    res.redirect('/matching/outcome-3-create')
+  }
+
+})
 // Add your routes here - above the module.exports line
 
 module.exports = router
