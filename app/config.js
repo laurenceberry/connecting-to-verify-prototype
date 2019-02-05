186e4af1c71b801f5c7291eaeb345453f44e827d
Chris Thomas <christopher.thomas@digital.cabinet-office.gov.uk>
2019-02-05 11:54:21 +0000

    Upgrade to prototype kit v8.6.0

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: '',
+  serviceName: 'Service name goes here',
 
   // Default port that prototype runs on
   port: '3000',
@@ -15,6 +15,10 @@ module.exports = {
   // Automatically stores form data, and send to all views
   useAutoStoreData: 'true',
 
+  // Enable cookie-based session store (persists on restart)
+  // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
+  useCookieSessionStore: 'false',
+
   // Enable or disable built-in docs and examples.
   useDocumentation: 'true',
 
@@ -22,7 +26,7 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a class="govuk-link" href="#">Find out more about cookies</a>',
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
   // Enable or disable Browser Sync
   useBrowserSync: 'true'
